e79e205092786efd91174359f8f920c1abbc5a18
devops-fourkites <devops@fourkites.com>
2018-10-14 06:49:02 +0000

    instruction modified

--- create_instance.sh
+++ create_instance.sh
@@ -16,7 +16,7 @@ SEC_GROUP_ID=sg-02d46a6c8119fb4dc
 OUT_FILE=.output.txt
 
 # instance creation
-aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE --key-name $KEY_NAME --security-group-ids $SEC_GROUP_ID > $OUT_FILE
+aws ec2 run-instances --image-id $IMAGE_ID --count 4 --instance-type $INSTANCE_TYPE --key-name $KEY_NAME --security-group-ids $SEC_GROUP_ID > $OUT_FILE
 sleep 60
 #aws ec2 create-tags --resources `cat .output.txt | grep InstanceId | awk '{print $2}' | cut -d'"' -f2` --tags Key=env,Value=hadoop_training && echo "Tag Added"
 
@@ -25,7 +25,7 @@ for i in `cat .output.txt | grep InstanceId | awk '{print $2}'  | cut -d'"' -f2`
 
 sleep 2
 
-echo -e "\tYou can use Instance IP and attached .pem key to login the servers.\n\nInstructions:\n\n1). Download the attached .pem key\n2). Set proper permision for .pem key: chmod 400 hadoop_admin.pem\n3. Username: ec2-user\n4. Login syntax: ssh -i "hadoop_admin.pem" ec2-user@YOUR_PUBLIC_IP\n" > .public_ip.txt
+echo -e "\tYou can use Instance IP and attached .pem key to login the servers.\n\nInstructions:\n\n1). Download the attached .pem key\n2). Set proper permission for .pem key: chmod 400 hadoop_admin.pem\n3). Username: ec2-user\n4). Login syntax: ssh -i "hadoop_admin.pem" ec2-user@YOUR_PUBLIC_IP\n" > .public_ip.txt
 
 # getting public ips
 for i in `cat .output.txt | grep InstanceId | awk '{print $2}'  | cut -d'"' -f2` ; do echo $i: `aws ec2 describe-instances --instance-ids $i | grep PublicIpAddress | awk -F: '{print $2}' | cut -d'"' -f2` ; done  >> .public_ip.txt
